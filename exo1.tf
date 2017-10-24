2bd77eab953a956da21627f062260ac804a2b929
EC2 Default User <ec2-user@ip-172-31-11-67.eu-west-2.compute.internal>
2017-10-24 09:42:51 +0000

    2 subnet

--- exo1.tf
+++ exo1.tf
@@ -6,4 +6,14 @@ resource "aws_vpc" "aws_rbtest" {
   cidr_block = "172.23.0.0/16"
 }
 
+resource "aws_subnet" "pubnet1" {
+  vpc_id            = "${aws_vpc.aws_rbtest.id}"
+  cidr_block        = "172.23.1.0/24"
+  availability_zone = "eu-west-1a"
+}
 
+resource "aws_subnet" "pubnet2" {
+  vpc_id            = "${aws_vpc.aws_rbtest.id}"
+  cidr_block        = "172.23.2.0/24"
+  availability_zone = "eu-west-1b"
+}
