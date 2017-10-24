eec93e5d0629b4105425b8a6316b7d186531dc73
EC2 Default User <ec2-user@ip-172-31-11-67.eu-west-2.compute.internal>
2017-10-24 10:10:54 +0000

    Step 2
    2 subnet
    route table

--- exo1.tf
+++ exo1.tf
@@ -6,10 +6,32 @@ resource "aws_vpc" "aws_rbtest" {
   cidr_block = "172.23.0.0/16"
 }
 
+resource "aws_internet_gateway" "gateway_rbtest" {
+  vpc_id            = "${aws_vpc.aws_rbtest.id}"
+}
+
+resource "aws_route_table" "route_internet" {
+  vpc_id            = "${aws_vpc.aws_rbtest.id}"
+  route {
+     cidr_block ="0.0.0.0/0"
+     gateway_id = "${aws_internet_gateway.gateway_rbtest.id}"
+  }
+}
+
+resource "aws_route_table_association" "route_subnet1" {
+  subnet_id = "${aws_subnet.pubnet1.id}"
+  route_table_id = "${aws_route_table.route_internet.id}"
+}
+
 resource "aws_subnet" "pubnet1" {
   vpc_id            = "${aws_vpc.aws_rbtest.id}"
   cidr_block        = "172.23.1.0/24"
   availability_zone = "eu-west-1a"
+}
+
+resource "aws_route_table_association" "route_subnet2" {
+  subnet_id = "${aws_subnet.pubnet2.id}"
+  route_table_id = "${aws_route_table.route_internet.id}"
 }
 
 resource "aws_subnet" "pubnet2" {
